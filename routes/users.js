81544079003bb8beb00a8e77607ecd332e289210
Grah Panchal <grahpanchal3110@gmail.com>
2024-09-28 15:54:46 +0530

    done

---
+++ routes/users.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const { type } = require('os');
+const plm = require("passport-local-mongoose");
+
+mongoose.connect('mongodb://127.0.0.1:27017/pint');
+
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  password: String,
+  profileImage: String,
+  contact: Number,
+  boards: {
+    type: Array,
+    default: []
+  },
+  posts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post'
+  }]
+}) 
+
+userSchema.plugin(plm);
+
+module.exports = mongoose.model('User', userSchema);
+
