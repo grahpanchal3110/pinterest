42128d8553a8657316d604682bf5efb36fdf165e
Grah Panchal <grahpanchal3110@gmail.com>
2024-09-28 16:43:43 +0530

    Some changes

--- api/app.js
+++ api/app.js
@@ -4,9 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const expressSession = require('express-session');
+const port = process.env.PORT || 3000;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('../routes/index');
+var usersRouter = require('../routes/users');
 const passport = require('passport');
 
 var app = express();
